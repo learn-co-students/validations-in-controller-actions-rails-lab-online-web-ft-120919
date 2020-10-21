7decfc8fb6fc5850747e8b0614ebeb63cc3a4e5f
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-10-21 11:34:42 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true;
+    validates :content, presence: true;
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction ) }
+
+
+    # def fic_or_nonfic
+    #     cat = "Fiction" || "Non-Fiction"
+    # end
 end
